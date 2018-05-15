e8d9320dba01fa9473733f762be50222e3965773
Debi Chowdhury <debi.chowdhury@learningmatekol.local>
2018-05-15 19:00:39 +0530

    login and signup UI completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,19 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { AgmCoreModule } from '@agm/core';
 import { LocationMapComponent } from './location-map/location-map.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LocationMapComponent
+    LocationMapComponent,
+    LoginComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
@@ -19,7 +25,16 @@ import { LocationMapComponent } from './location-map/location-map.component';
     FormsModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyA0PZkCm7Ic8RDDVEt_3j2xjts5OD2xN_Q'
-    })
+    }),
+    RouterModule.forRoot([
+      {
+        path: '', component: LoginComponent
+      }, {
+        path: 'signup', component: SignUpComponent
+      },
+      {
+        path: 'getlocation', component: LocationMapComponent
+    }])
   ],
   providers: [],
   bootstrap: [AppComponent]
