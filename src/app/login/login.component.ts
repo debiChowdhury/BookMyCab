d5c1fca86f08cca02955b9b8b729d07c7e704a14
Debarati Purkait <debarati.purkait@learningmate.com>
2018-05-16 18:18:22 +0530

    login functionality added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  userEmail: any;
+  userPassword: any;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onClickLoginButton() {
+    console.log('login credentials >>', this.userEmail, this.userPassword);
+    const userCredentials = {'username' : this.userEmail, 'password': this.userPassword};
+    localStorage.setItem('logged In User', JSON.stringify(userCredentials));
   }
 
 }
